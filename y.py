c9fc19bbe93ffacc436adf62de7f6399ef8b8e82
heiaiyou5 <132822176+heiaiyou5@users.noreply.github.com>
2024-07-04 14:07:01 +0800

    Update y.py

--- y.py
+++ y.py
@@ -1,1 +1,18 @@
-list
+"""
+Numbers
+"""
+
+"""
+Texts
+"""
+
+
+"""
+Lists
+"""
+# Fibonacci series:
+# the sum of two elements defines the next
+a, b = 0, 1
+while a < 10:
+    print(a)
+    a, b = b, a+b
